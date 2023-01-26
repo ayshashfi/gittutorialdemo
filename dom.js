60d99ef8bff089bbf3752568aedc54f510caba74
ayshashfi <ayshathashfiyaa@gmail.com>
2023-01-25 21:45:42 -0800

    added getelementbytagname

--- dom.js
+++ dom.js
@@ -24,16 +24,23 @@ console.log(document.links);
 
 //getElementByClass
 
-var items=document.getElementsByClassName('list-group-item');
+ var items=document.getElementsByClassName('list-group-item');
 console.log(items);
-console.log(items[1]);
-items[1].textContent='hello';
-items[1].style.fontWeight='bold';
-items[2].style.backgroundColor='green';
+// console.log(items[1]);
+// items[1].textContent='hello';
+// items[1].style.fontWeight='bold';
+// items[2].style.backgroundColor='green';
 
-//gives error
-//items.style.fontWeight-'bold';
+// //gives error
+// //items.style.fontWeight-'bold';
 
-for (var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-}
+// for (var i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+// }
+
+//getElementByTagName
+var li=document.getElementsByTagName('li');
+console.log(li);
+for (var i=0;i<li.length;i++){
+     li[i].style.backgroundColor='yellow';
+ }
